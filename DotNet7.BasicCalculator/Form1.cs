630060a4430224344d391f9e9078c8858e1d4489
Thet Htar Thu Zar <thethtarthuzar648@gmail.com>
2024-11-26 18:01:05 +0630

    feat: fix code

--- DotNet7.BasicCalculator/Form1.cs
+++ DotNet7.BasicCalculator/Form1.cs
@@ -184,7 +184,6 @@ namespace DotNet7.BasicCalculator
                 MessageBox.Show("Invalid Expression");
             }
         }
-
         private void btnClear_Click(object sender, EventArgs e)
         {
             txtResult.Clear();
